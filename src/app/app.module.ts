c8e6f8a5f3600007560b34b395e5a225bf6e98be
Luciano Penha <lucianopenha14@gmail.com>
2022-04-13 08:26:55 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { MuralVagasComponent } from './views/mural-vagas/mural-vagas.component';
+import { CadastoVagasComponent } from './views/cadasto-vagas/cadasto-vagas.component';
+import { CadastroVagasComponent } from './views/cadastro-vagas/cadastro-vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    MuralVagasComponent,
+    CadastoVagasComponent,
+    CadastroVagasComponent
   ],
   imports: [
     BrowserModule,
